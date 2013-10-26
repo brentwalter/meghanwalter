2ca30a16879ae1fc3f65275edaf86959b32abae3
Brent Walter <brent@brentwalter.com>
2013-10-26 13:15:28 -0400

    update WP to 3.7

--- wp-includes/update.php
+++ wp-includes/update.php
@@ -17,7 +17,7 @@
  * @since 2.3.0
  * @uses $wp_version Used to check against the newest WordPress version.
  *
- * @param array $extra_stats Extra statistics to report to the WordPress.org API. 
+ * @param array $extra_stats Extra statistics to report to the WordPress.org API.
  * @return mixed Returns null if update is unsupported. Returns false if check is too soon.
  */
 function wp_version_check( $extra_stats = array() ) {
@@ -415,9 +415,6 @@ function wp_maybe_auto_update() {
 	include_once ABSPATH . '/wp-admin/includes/class-wp-upgrader.php';
 
 	$upgrader = new WP_Automatic_Updater;
-	if ( $upgrader->is_disabled() )
-		return;
-
 	$upgrader->run();
 }
 
